63c7a56bd65440167512971f630fa9695b846d11
Paul Peter Callaghan <calgahan@live.co.uk>
2024-09-12 14:45:31 +0100

    changed things

---
+++ exercise5.py
@@ -0,0 +1,7 @@
+number_guess = int(input("Guess the number?"))
+magic_number=21
+
+if number_guess == magic_number:
+    print("Congratulations! You guessed the correct number.")
+else:
+    print(f"Sorry, the correct number is {magic_number}.")
