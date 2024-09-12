f08e27285e5790091b5fb8136b85d6fe1d4f1e75
Paul Peter Callaghan <calgahan@live.co.uk>
2024-09-12 11:12:17 +0100

    challange two task 6

--- tut.py
+++ tut.py
@@ -1,8 +1,5 @@
-import datetime
-
-def displaydate(now = "paul"):
-    now = "De"
-    print(now)
-
-
-displaydate()
+foods = ['cheese', 'eggs', 'bread', 'milk', 'apples', 'orange']
+print(foods)
+print(foods.sort(reverse=True))
+for food in foods:
+    print(food.title())
