749079bd424931df59b49e95bf2309e5b0d6d87f
Paul Peter Callaghan <calgahan@live.co.uk>
2024-09-12 14:04:59 +0100

    exercise 4 complete code

--- tut.py
+++ tut.py
@@ -1,5 +1,53 @@
-foods = ['cheese', 'eggs', 'bread', 'milk', 'apples', 'orange']
+'''foods = ['cheese', 'eggs', 'bread', 'milk', 'apples', 'orange']
 print(foods)
 print(foods.sort(reverse=True))
 for food in foods:
     print(food.title())
+'''
+
+#exercise 4 for today task 2
+'''pizzas = ['Cheese', 'Meatballs', 'stuff crust']
+for pizza in pizzas:
+    print(f"I really like {pizza}")
+
+print("Me love pizza yum yum")'''
+
+#exercise 4 task 3
+'''Animals = ['Elephant', 'Lion', 'Girraffee']
+
+for animal in Animals:
+    print(f"I love watching {animal}s roar")
+
+print("All these animals love hot weather")'''
+
+#exercise 4 task 4
+'''numbers=range(1, 1000001)
+
+print(sum(numbers))
+print(min(numbers))
+print(max(numbers))'''
+
+#exercise 4 task 5
+'''
+for number in range(3, 30 ,3):
+    print(number)
+'''
+
+#exercise 4 task 6
+'''
+pizzas = ['Cheese', 'Meatballs', 'stuff crust']
+friends_pizzas = ['Cheese', 'Meatballs', 'stuff crust', 'Chicken']
+
+for pizza in pizzas:
+    print(f"My favouriate pizzas are {pizza}")
+
+for friend_pizza in friends_pizzas:
+    print(f"One of my friends favourite pizzas is {friend_pizza}")
+'''
+
+#exercise 4 task 7
+menu_foods=('Chicken Balls', 'Sushi', 'Chips', 'Pasta', 'Pizza')
+new_items=('deserts', 'Bread')
+revised_menu=menu_foods+new_items
+for food in revised_menu:
+    print(f"I would like to eat {food}")
