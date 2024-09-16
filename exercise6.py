68b2350dfad110dd0a59f71a6716246b9fecaae5
Paul Peter Callaghan <calgahan@live.co.uk>
2024-09-16 11:25:42 +0100

    quick git commit

--- exercise6.py
+++ exercise6.py
@@ -1,4 +1,5 @@
 #Exercise 6 Task 1
+'''
 Enya = {
     "first_name":"Enya",
     "last_name":"Ryan",
@@ -17,3 +18,84 @@ people=[Enya,Paul]
 
 for person in people:
     print(f"{person['first_name']} {person['last_name']} is {person['age']} years old and lives in {person['location']}.")
+'''
+
+#Exercise 6 Task 2
+'''
+numbers = {
+    "Enya":23,
+    "Paul":25,
+    "James":28,
+    "Pat":27,
+    "Snowball":26,
+}
+
+for name, fav_number in numbers.items():
+    print(f"{name} favoirate number is {fav_number}.")
+'''
+
+#Exercise 6 Task 3
+'''
+rivers = {
+    "Thames": "England",
+    "Nile": "Egypt",
+    "Amazon": "South America",
+    "Mississippi": "North America",
+    "Yellow": "Asia",
+}
+
+for river, country in rivers.items():
+    print(f"{river} flows through {country}.")
+
+for river_name in rivers.keys():
+    print(f"{river_name}")
+
+for country_name in rivers.values():
+    print(f"{country_name}")
+'''
+
+#Exercise 6 Task 4
+'''
+snowball = {
+    "animal":"cat",
+    "owner":"Paul"
+}
+
+garfield = {
+    "animal":"dog",
+    "owner":"Enya"
+}
+
+grey ={
+    "animal":"parrot",
+    "owner":"Peter"
+}
+
+wrogger = {
+    "animal":"rabbit",
+    "owner":"James"
+}
+
+timmy = {
+    "animal":"tiger",
+    "owner":"Pat"
+}
+
+fuzzy = {
+    "animal":"mouse",
+    "owner":"Dan"
+}
+
+carloze = {
+    "animal":"hamster",
+    "owner":"David"
+}
+
+pets = [snowball, garfield, grey, wrogger, timmy, fuzzy, carloze]
+
+for pet in pets:
+    print(f"{pet['owner']} has a {pet['animal']} called pet.")
+'''
+
+#Exercise 6 Task 5
+
